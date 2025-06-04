0a08483ee70d4be7c0c288568cc9a160bbfe7ee5
Shatunov Evgeny <shatunov868@gmail.com>
2025-06-04 16:07:15 +0300

    chore: Add simclr to config

--- src/model/hparams.py
+++ src/model/hparams.py
@@ -1,6 +1,10 @@
+from pathlib import Path
+
+PROJECT_ROOT = Path(__file__).parent.parent.parent
+
 config = dict(
-    n_way=5,
-    n_support=1,
+    n_way=1,
+    n_support=5,
     n_query=5,
     max_epoch=5,
     epoch_size=2000,
@@ -9,4 +13,6 @@ config = dict(
     hid_dim=64,
     z_dim=64,
     augment_flag=True,
+    use_simclr=True,
+    simclr_path = PROJECT_ROOT / "model" / "SimCLR" / "checkpoints" / "best.pt",
 )
